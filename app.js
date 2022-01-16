7ced6c53f4c8f85127489e7153d447ca6b0a074b
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2022-01-16 20:30:04 +0330

    create todolist

---
+++ app.js
@@ -0,0 +1,90 @@
+const newlist = document.querySelector('.addlist');
+const btnadd = document.querySelector('.btn_add');
+const input = document.querySelector('.todo-input');
+
+btnadd.addEventListener('click', added);
+newlist.addEventListener("click",removeItem)
+function added(e) {
+    if (input.value !='') {
+        const div = document.createElement('div');
+        const li = document.createElement('li');
+        const btnE = document.createElement('button');
+        const btnD = document.createElement('button');
+        btnE.innerHTML = '<li class="fas fa-edit"></li>';
+        btnD.innerHTML = '<li class="fas fa-trash"></li>';
+        li.classList.add('li');
+        btnE.classList.add('btnE');
+        btnD.classList.add('btnD');
+        li.innerHTML =input.value;
+        div.appendChild(btnE);
+        div.appendChild(btnD);
+        li.appendChild(div)
+        newlist.appendChild(li)
+        adding(input.value);
+        input.value = '';
+       
+    }
+    e.preventDefault();
+}
+
+
+function adding(e) {
+    let todos;
+    if (localStorage.getItem('todo') === null) {
+        todos = [];
+    }
+    else {
+        todos=JSON.parse(localStorage.getItem('todo'))
+    }
+    todos.push(e)
+    localStorage.setItem('todo',JSON.stringify(todos)); 
+}
+
+function setfocus() {
+    const h = localStorage.getItem('todo');
+    const res = JSON.parse(h)
+    res.forEach(P => {
+        const div = document.createElement('div');
+        const li = document.createElement('li');
+        const btnE = document.createElement('button');
+        const btnD = document.createElement('button');
+        btnE.innerHTML = '<li class="fas fa-edit"></li>';
+        btnD.innerHTML = '<li class="fas fa-trash"></li>';
+        li.classList.add('li');
+        btnE.classList.add('btnE');
+        btnD.classList.add('btnD');
+        li.innerHTML = P ;
+        div.appendChild(btnE);
+        div.appendChild(btnD);
+        li.appendChild(div)
+        newlist.appendChild(li)
+        input.innerHTML = res[0]
+    });
+}
+setfocus()
+
+function removeItem(e) {
+    const item = e.target;
+    if (item.classList[0] === 'btnD') {
+        const d = item.parentElement.parentElement;
+        d.remove();
+    }
+   removeLocal(e)
+}
+
+function removeLocal(e) {
+    const item = e.target;
+    const newi = item.parentElement.parentElement
+    console.log(newi);
+       let todos;
+       if (localStorage.getItem('todo') === null) {
+               todos = [];
+           } else {
+           todos = JSON.parse(localStorage.getItem('todo'))
+            }
+    console.log(todos.indexOf(newi.innerText));
+    let resl = todos.indexOf(newi)
+    todos.splice(resl,1)
+    localStorage.setItem('todo', JSON.stringify(todos));
+    
+}
